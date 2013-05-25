03d9366e1b4b64139cc0ce216582b350aa551c01
Terry Tong <blessedoverlap@gmail.com>
2013-05-25 11:50:44 +0800

    Minor changes

---
+++ event-model/fire-count.js
@@ -0,0 +1,11 @@
+var fireCount = 0;
+var start = new Date;
+
+var timer = setInterval(function() {
+  if (new Date - start > 1000) {
+    clearInterval(timer);
+    console.log(fireCount);
+    return;
+  }
+  fireCount++;
+}, 0);
